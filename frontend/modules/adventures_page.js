80d67a41693524ff45e938c273b3a74a2e55a32f
Anand <pie1002anand@gmail.com>
2023-02-20 20:53:56 +0530

    ME_QTRIPDYNAMIC_MODULE_DEPLOYMENT

--- frontend/modules/adventures_page.js
+++ frontend/modules/adventures_page.js
@@ -1,43 +1,40 @@
-
 import config from "../conf/index.js";
 
 //Implementation to extract city from query params
 function getCityFromURL(search) {
   // TODO: MODULE_ADVENTURES
   // 1. Extract the city id from the URL's Query Param and return it
-  return new URLSearchParams(search).get('city')
-
-
+  return new URLSearchParams(search).get("city");
 }
 
 //Implementation of fetch call with a paramterized input based on city
 async function fetchAdventures(city) {
   // TODO: MODULE_ADVENTURES
   // 1. Fetch adventures using the Backend API and return the data
-  let adventures = []
+  let adventures = [];
   try {
-    adventures = await fetch(`${config.backendEndpoint}/adventures/?city=${city}`).then((res) => {
-      return res.json()
-    })
+    adventures = await fetch(
+      `${config.backendEndpoint}/adventures/?city=${city}`
+    ).then((res) => {
+      return res.json();
+    });
   } catch (e) {
-    return null
+    return null;
   }
 
-  return adventures
-
+  return adventures;
 }
 
 //Implementation of DOM manipulation to add adventures for the given city from list of adventures
 function addAdventureToDOM(adventures) {
   // TODO: MODULE_ADVENTURES
   // 1. Populate the Adventure Cards and insert those details into the DOM
-  const container = document.getElementById('data')
+  const container = document.getElementById("data");
 
   adventures.forEach((item) => {
-
     container.innerHTML += `
     <div class="  col-lg-3 col-xs-6 col-sm-6 col-md-6 mb-2">
-    <a href="/frontend/pages/adventures/detail/?adventure=${item.id}" class="activity-card" id=${item.id}>
+    <a href="/pages/adventures/detail/?adventure=${item.id}" class="activity-card" id=${item.id}>
     <div class="category-banner">${item.category}</div>
       <img src=${item.image} class="activity-card-image"/>
       
@@ -50,41 +47,35 @@ function addAdventureToDOM(adventures) {
         <p>${item.duration} Hours</p>
       </div>
     </a>
-  </div>`
-
-  })
-
-
-
+  </div>`;
+  });
 }
 
 //Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
 function filterByDuration(list, low, high) {
   // TODO: MODULE_FILTERS
   // 1. Filter adventures based on Duration and return filtered list
-  let filteredList = []
+  let filteredList = [];
 
   list.forEach((item) => {
     if (item.duration >= low && item.duration <= high) {
-      filteredList.push(item)
+      filteredList.push(item);
     }
-  })
+  });
   return filteredList;
-
 }
 
 //Implementation of filtering by category which takes in a list of adventures, list of categories to be filtered upon and returns a filtered list of adventures.
 function filterByCategory(list, categoryList) {
   // TODO: MODULE_FILTERS
   // 1. Filter adventures based on their Category and return filtered list
-  let filteredList = []
+  let filteredList = [];
 
   list.forEach((item) => {
-    if (categoryList.includes(item.category)) filteredList.push(item)
-  })
+    if (categoryList.includes(item.category)) filteredList.push(item);
+  });
 
-  return filteredList
-
+  return filteredList;
 }
 
 // filters object looks like this filters = { duration: "", category: [] };
@@ -100,28 +91,25 @@ function filterFunction(list, filters) {
   // 2. Depending on which filters are needed, invoke the filterByDuration() and/or filterByCategory() methods
 
   const categoryFlag = filters.category.length > 0 ? true : false;
-  const durationFlag = filters.duration === "" ? false : true
-  let s = 0, e = 0
+  const durationFlag = filters.duration === "" ? false : true;
+  let s = 0,
+    e = 0;
 
   if (durationFlag) {
     const arr = filters.duration.split("-");
 
-    s = arr[0]
-    e = arr[1]
+    s = arr[0];
+    e = arr[1];
   }
-
 
   if (categoryFlag && durationFlag) {
-    list = filterByCategory(list, filters.category)
-    list = filterByDuration(list, s, e)
+    list = filterByCategory(list, filters.category);
+    list = filterByDuration(list, s, e);
   } else if (categoryFlag && !durationFlag) {
-    list = filterByCategory(list, filters.category)
+    list = filterByCategory(list, filters.category);
   } else if (!categoryFlag && durationFlag) {
-    list = filterByDuration(list, s, e)
+    list = filterByDuration(list, s, e);
   }
-
-
-
 
   // Place holder for functionality to work in the Stubs
   return list;
@@ -131,7 +119,7 @@ function filterFunction(list, filters) {
 function saveFiltersToLocalStorage(filters) {
   // TODO: MODULE_FILTERS
   // 1. Store the filters as a String to localStorage
-  localStorage.setItem('filters', JSON.stringify(filters))
+  localStorage.setItem("filters", JSON.stringify(filters));
 
   return true;
 }
@@ -140,8 +128,7 @@ function saveFiltersToLocalStorage(filters) {
 function getFiltersFromLocalStorage() {
   // TODO: MODULE_FILTERS
   // 1. Get the filters from localStorage and return String read as an object
-  const filters = JSON.parse(localStorage.getItem('filters'))
-
+  const filters = JSON.parse(localStorage.getItem("filters"));
 
   // Place holder for functionality to work in the Stubs
   return filters;
@@ -154,14 +141,13 @@ function getFiltersFromLocalStorage() {
 function generateFilterPillsAndUpdateDOM(filters) {
   // TODO: MODULE_FILTERS
   // 1. Use the filters given as input, update the Duration Filter value and Generate Category Pills
-  const filterContainer = document.getElementById('category-list')
+  const filterContainer = document.getElementById("category-list");
 
   filters.category.forEach((item) => {
     filterContainer.innerHTML += `<div class="category-filter">
     ${item}
-    </div>`
-  })
-
+    </div>`;
+  });
 }
 export {
   getCityFromURL,
--- frontend/modules/reservation_page.js
+++ frontend/modules/reservation_page.js
@@ -4,35 +4,54 @@ import config from "../conf/index.js";
 async function fetchReservations() {
   // TODO: MODULE_RESERVATIONS
   // 1. Fetch Reservations by invoking the REST API and return them
-  let reservations = []
+  let reservations = [];
   try {
-    reservations = await fetch(`${config.backendEndpoint}/reservations`).then((res) => {
-      return res.json()
-    })
+    reservations = await fetch(`${config.backendEndpoint}/reservations`).then(
+      (res) => {
+        return res.json();
+      }
+    );
   } catch (e) {
-    return null
+    return null;
   }
-
 
   // Place holder for functionality to work in the Stubs
   return reservations;
 }
 
 function getDateString(d) {
-  const date = new Date(d)
+  const date = new Date(d);
 
-
-  const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
 
-  return date.getDate() + " " + monthNames[date.getMonth()] + " " + date.getFullYear() + ", " + date.toLocaleTimeString('en-IN')
+  return (
+    date.getDate() +
+    " " +
+    monthNames[date.getMonth()] +
+    " " +
+    date.getFullYear() +
+    ", " +
+    date.toLocaleTimeString("en-IN")
+  );
 }
 
 function getDateStr(date) {
-  const arr = date.split('-')
+  const arr = date.split("-");
 
-  return parseInt(arr[2]) + "/" + parseInt(arr[1]) + "/" + arr[0]
+  return parseInt(arr[2]) + "/" + parseInt(arr[1]) + "/" + arr[0];
 }
 
 //Function to add reservations to the table. Also; in case of no reservations, display the no-reservation-banner, else hide it.
@@ -40,14 +59,14 @@ function addReservationToTable(reservations) {
   // TODO: MODULE_RESERVATIONS
   // 1. Add the Reservations to the HTML DOM so that they show up in the table
   if (reservations.length === 0) {
-    document.getElementById('no-reservation-banner').style.display = "block"
-    document.getElementById('reservation-table-parent').style.display = "none"
-    return
+    document.getElementById("no-reservation-banner").style.display = "block";
+    document.getElementById("reservation-table-parent").style.display = "none";
+    return;
   } else {
-    document.getElementById('no-reservation-banner').style.display = "none"
-    document.getElementById('reservation-table-parent').style.display = "block"
+    document.getElementById("no-reservation-banner").style.display = "none";
+    document.getElementById("reservation-table-parent").style.display = "block";
   }
-  const table = document.getElementById('reservation-table')
+  const table = document.getElementById("reservation-table");
 
   reservations.forEach((item) => {
     table.innerHTML += ` <tr>
@@ -58,9 +77,13 @@ function addReservationToTable(reservations) {
         <td>${getDateStr(item.date)}</td>
         <td>${item.price}</td>
         <td>${getDateString(item.time)}</td>
-        <td><button id=${item.id} class="reservation-visit-button"><a href="../detail/?adventure=${item.adventure}">Visit Adventure</a></button></td>
-      </tr>`
-  })
+        <td><button id=${
+          item.id
+        } class="reservation-visit-button"><a href="/pages/detail/?adventure=${
+      item.adventure
+    }">Visit Adventure</a></button></td>
+      </tr>`;
+  });
   //Conditionally render the no-reservation-banner and reservation-table-parent
 
   /*
@@ -71,8 +94,6 @@ function addReservationToTable(reservations) {
     1. The date of adventure booking should appear in the format D/MM/YYYY (en-IN format) Example:  4/11/2020 denotes 4th November, 2020
     2. The booking time should appear in a format like 4 November 2020, 9:32:31 pm
   */
-
-
 }
 
 export { fetchReservations, addReservationToTable };
